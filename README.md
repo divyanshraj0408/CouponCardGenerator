66e4897995712e27e6c77d606bb6e97897f212ea
Divyansh <divyanshraj0408@gmail.com>
2024-06-29 03:01:50 +0530

    update Readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Coupon code generator
+
+<!-- Evaluation Criteria:
+Functionality: The website should meet the basic functional requirements.
+Code Quality: Clean and well-documented code.
+Design and Usability: Simple, clear, and user-friendly interface.
+Completion: Ability to complete the task within the given timeframe.
+ -->
+
+### Install and usage
+
+- Fork the project or download it
+- - If you have forked it then clone the project using
+- - `git clone "your-repo-link"`
+- Go to the directory and either run `npm serve` or use live server extension on vscode to see the website live.
+
+### Structure of the project
+
+This project folder contains
+
+- index.html
+- styles.css
+- script.js
+
+  The coupon code generating logic is in the `updateCouponPreview()` function .
+
+  - In the fucntion after getting all the input fields, we set up the canvas with a background image. Also setting up the font style and font size
+  - Then we generate a coupon code which is made of `The organisation name + a random number`, and then a barcode is generated based on that code with the help of [jsBarcode library](https://lindell.me/JsBarcode/).
+  - Then we show the all the input variables and coupon code on the canvas, then we can download the coupon as `png` using the download button.
--- script.js
+++ script.js
@@ -1,40 +1,47 @@
+//to update the coupon once the content is loaded on the page.
 document.addEventListener("DOMContentLoaded", function () {
   updateCouponPreview();
 });
 
+//to update the coupon
 function updateCouponPreview() {
+  //getting the input fields.
   const orgName = document.getElementById("orgName").value;
   const deal = document.getElementById("deal").value;
   const expiryDate = document.getElementById("expiryDate").value;
-
   const canvas = document.getElementById("couponCanvas");
+
+  //setting up canvas components with the background image.
   const context = canvas.getContext("2d");
   const backgroundImage = new Image();
   backgroundImage.src = "background.jpg";
 
+  //upon the loading of the background image of canvas:
   backgroundImage.onload = function () {
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-
     context.fillStyle = "#000";
     context.font = "20px Open Sans";
     context.fillText("Congratulations 🎉", 20, 30);
+    context.font = "16px Open Sans";
 
-    context.font = "16px Arial";
-    const string =
+    //setting the coupon code.
+    const code =
       `${orgName.toUpperCase().replaceAll(/\s/g, "")}` +
       Math.round(Math.random() * 10000);
+
     if (orgName) {
       context.fillText(
         `You won a coupon from ${orgName.toUpperCase()}.`,
         20,
         70
       );
-      context.fillText(`${string}`, 450, 190);
+      context.fillText(`${code}`, 450, 190);
     }
+    //showing the barcode
     if (orgName) {
       const barcodeCanvas = document.createElement("canvas");
-      JsBarcode(barcodeCanvas, string, {
+      JsBarcode(barcodeCanvas, code, {
         format: "CODE128",
         width: 1,
         height: 50,
@@ -42,6 +49,7 @@ function updateCouponPreview() {
       });
       context.drawImage(barcodeCanvas, 450, 100);
     }
+
     if (deal)
       context.fillText(`Which gives you ${deal.toUpperCase()}.`, 20, 110);
     if (expiryDate)
@@ -54,7 +62,7 @@ function updateCouponPreview() {
     }
   };
 }
-
+// to download the coupon
 function downloadCoupon() {
   const canvas = document.getElementById("couponCanvas");
   const link = document.createElement("a");
